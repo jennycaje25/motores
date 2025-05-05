e291cc3daa839267d66b8e04843314db1195ba2c
jennycaje25 <jennifer.gabrielle@escolar.ifrn.edu.br>
2025-05-05 12:09:58 -0300

    princess

---
+++ motores/motorespjd1m/Assets/Scenes/script/Play.cs
@@ -0,0 +1,44 @@
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class Play : MonoBehaviour
+{
+    
+    
+    
+    private int numero;
+    public float velocidade = 40;
+    
+    void Start()
+    {
+        numero = 0;
+        
+    }
+
+
+    void Update()
+    {
+        //Debug.Log(numero);
+        //numero = numero + 1; 
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            gameObject.transform.position += new Vector3(velocidade * Time.deltaTime, 0, 0);
+
+        }
+
+        if (Input.GetKey(KeyCode.D))
+            
+            gameObject.transform.position += new Vector3(velocidade * Time.deltaTime, 0, 0);
+        {
+        }
+
+
+
+    }
+}
+               
+        
+        
+        
+         
