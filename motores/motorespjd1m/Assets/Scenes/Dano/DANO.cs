f2061d2c5b0d32878d13851a42458d1411af8b89
jennycaje25 <jennifer.gabrielle@escolar.ifrn.edu.br>
2025-09-23 10:26:21 -0300

    elementos

---
+++ motores/motorespjd1m/Assets/Scenes/Dano/DANO.cs
@@ -0,0 +1,14 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DANO : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);          
+        }
+    }
+}
